4e7dfdaa81c3beb44ff1b0ad9d325c61d3d929d5
mccormi <emccormick@cedarville.edu>
2016-03-20 18:17:58 -0400

    Completed parse function for expression

--- project1/src/compiler/parser/Expression.java
+++ project1/src/compiler/parser/Expression.java
@@ -1,6 +1,8 @@
 package compiler.parser;
 
-public abstract class Expression {
+import compiler.scanner.Token;
+
+public class Expression {
 	private Integer num;
 	private SimpleExpressionP simpleExpressionP;
 	private Expression expression;
@@ -24,8 +26,31 @@ public abstract class Expression {
 	
 	public static Expression parseExpression(Parser parser) 
 			throws ParserException{
-
-		switch 
-
+		
+		Token currentToken = parser.getCurrentToken();
+		Expression expression;
+		SimpleExpressionP sep;
+		
+		switch(currentToken.getTokenType()){
+			case NUM_TOKEN:
+				Integer n = (Integer) currentToken.getTokenData();
+				sep = SimpleExpressionP.parseSimpleExpresionP(parser);
+				expression = new Expression (n, sep);
+				return expression;
+			case LEFTPAREN_TOKEN:
+				Expression innerExpression = Expression.parseExpression(parser);
+				sep = SimpleExpressionP.parseSimpleExpresionP(parser);
+				expression = new Expression (innerExpression, sep);
+				return expression;
+			case ID_TOKEN:
+				currentToken = parser.getCurrentToken();
+				String ident = (String) currentToken.getTokenData();
+				ExpressionP ep = ExpressionP.parseExpressionP(parser);
+				expression = new Expression (ident, ep);
+				return expression;
+			default:
+				throw new ParserException("Parse exception occured while parsing expression'");
+				
+		}
 	}
 }

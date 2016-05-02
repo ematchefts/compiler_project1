02679ad31da59fbbd681d83686f53b5012e00b04
mccormi <emccormick@cedarville.edu>
2016-05-02 13:39:58 -0400

    Realized that my abstract tree printing was actually removing parsing objects as it was printing.

--- project1/exampleProgram1.s
+++ project1/exampleProgram1.s
@@ -3,10 +3,24 @@
 .globl  gcd
 gcd:
 gcd_bb2:
+	movl	%ESI, %EAX
+gcd_bb3:
+	cmpl	$0, %EAX
+	jne	gcd_bb6
+gcd_bb4:
+	movl	%EDI, %EAX
 gcd_bb1:
 	ret
+gcd_bb6:
+	call	gcd
+	jmp	gcd_bb1
+	jmp	gcd_bb1
 .globl  main
 main:
 main_bb2:
+main_bb3:
+	call	input
+	call	input
+	call	output
 main_bb1:
 	ret
